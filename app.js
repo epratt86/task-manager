d8f7a4d5fdb793fd067f7472754e32a67ae2e8ee
Eric Pratt <ericpratt86@gmail.com>
2018-12-04 13:00:14 -0800

    add filter functionality

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function loadEventListeners() {
   taskList.addEventListener("click", removeTask);
   //clear all tasks
   clearBtn.addEventListener("click", clearTasks);
+  //filter through the tasks
+  filter.addEventListener("keyup", filterTasks);
 }
 
 //addTask function
@@ -21,26 +23,27 @@ function addTask(e) {
   //if no task is inputed BUT user trys to submit, show alert message
   if (taskInput.value === "") {
     alert("Please add a task");
-  }
-  //now that input is not blank - add it to the UI
-  const li = document.createElement("li");
-  //add class
-  li.className = "collection-item";
-  //create a text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  //create new link element
-  const link = document.createElement("a");
-  //add class
-  link.className = "delete-item secondary-content";
-  //add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //append the link to li
-  li.appendChild(link);
-  //append the li to the ul to display in DOM
-  taskList.appendChild(li);
+  } else {
+    //now that input is not blank - add it to the UI
+    const li = document.createElement("li");
+    //add class
+    li.className = "collection-item";
+    //create a text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement("a");
+    //add class
+    link.className = "delete-item secondary-content";
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to li
+    li.appendChild(link);
+    //append the li to the ul to display in DOM
+    taskList.appendChild(li);
 
-  //clear the input on submission
-  taskInput.value = "";
+    //clear the input on submission
+    taskInput.value = "";
+  }
 
   // prevent the form submission behavior
   e.preventDefault();
@@ -56,6 +59,21 @@ function removeTask(e) {
       e.target.parentElement.parentElement.remove();
     }
   }
+}
+//Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  //look for all items in collection
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    //if the item matches the index of what is being filter - it will return '1', '-1' is if there is no match
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
 
 //clearTask function
